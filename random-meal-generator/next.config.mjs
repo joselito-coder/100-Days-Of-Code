931cadebd59b50ba9fb16ffa66f3d6ade57346a1
Joselito <joselitorebello20@gmail.com>
2025-05-28 23:58:13 +0530

    Remove old directory for random meal generator

--- random-meal-generator/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [new URL("https://www.themealdb.com/images/media/meals/**")
-        ]
-    },
-};
-
-export default nextConfig;
